c65c4a66a91497dd13ad69d3ed2d6cf2ea742228
IanSchillewaert <ian.schillewaert@telenet.be>
2018-08-03 16:00:16 +0200

    init

---
+++ LokalenVerdeling/LokalenVerdeling/Model/Activiteit.swift
@@ -0,0 +1,9 @@
+//
+//  Activiteit.swift
+//  LokalenVerdeling
+//
+//  Created by Ian Schillewaert on 03/08/2018.
+//  Copyright © 2018 Ian Schillewaert. All rights reserved.
+//
+
+import Foundation
---
+++ LokalenVerdeling/LokalenVerdeling/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LokalenVerdeling
+//
+//  Created by Ian Schillewaert on 03/08/2018.
+//  Copyright © 2018 Ian Schillewaert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
