7d87f249b91f398ac69aa2aa4eec1b62b812a1ca
IanSchillewaert <ian.schillewaert@telenet.be>
2018-08-06 22:16:33 +0200

    small commit

---
+++ LokalenVerdeling/LokalenVerdeling/Cells/SessionTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  SessionTableViewCell.swift
+//  LokalenVerdeling
+//
+//  Created by Ian Schillewaert on 06/08/2018.
+//  Copyright © 2018 Ian Schillewaert. All rights reserved.
+//
+
+import UIKit
+
+class SessionTableViewCell: UITableViewCell {
+
+
+}
